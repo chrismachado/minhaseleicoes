8f7a9958b4046ff73fdfa97cd8b9702f1fbac161
unknown <christianomachado10@gmail.com>
2025-02-16 00:41:25 -0300

    Add MainMenu and Chart screens to navigation; update initial route to Welcome

--- frontend/minhaseleicoes/src/App.tsx
+++ frontend/minhaseleicoes/src/App.tsx
@@ -3,6 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import WelcomeScreen from '@screens/WelcomeScreen';
 import ResultsScreen from '@screens/ResultsScreen';
+import ChartScreen from '@screens/ChartScreen';
+import MainMenuScreen from '@screens/MainMenuScreen';
+
 
 const Stack = createStackNavigator();
 
@@ -10,16 +13,26 @@ export default function App() {
 
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Results">
+        <Stack.Navigator initialRouteName="Welcome">
           <Stack.Screen 
           name="Welcome" 
           component={WelcomeScreen} 
           options={{ title: 'Boas-vindas' }}
           />
           <Stack.Screen 
+            name="MainMenu" 
+            component={MainMenuScreen} 
+            options={{ title: 'Menu principal' }}
+          />
+          <Stack.Screen 
             name="Results"
             component={ResultsScreen}
             options={{ title: 'Resultados' }}
+          />
+          <Stack.Screen 
+            name="Chart"
+            component={ChartScreen}
+            options={{title: 'Gráficos'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
